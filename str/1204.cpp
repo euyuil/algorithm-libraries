3e4d28e111f671246897da74b6bb87329eee56fa
Liu Yue <euyuil@acm.org>
2011-08-15 08:36:23 +0800

    Added some comments to class trie.

--- str/1204.cpp
+++ str/1204.cpp
@@ -6,213 +6,103 @@
 
 using namespace std;
 
+namespace gatelib {
+
+namespace str {
+
+/**
+ * @brief A structure that can match a text with serveral patterns.
+ * @date 2011-08-14
+ */
+
 class trie {
 private:
-	struct node {
-		size_t prev, paid, edge[26];
-		bool danger, target;
-		node() { memset(this, 0, sizeof(*this)); }
-		size_t &operator[](char a) { return edge[a - 'A']; }
-	};
+    struct node {
+        size_t prev, paid, edge[26];
+        bool danger, target;
+        node() { memset(this, 0, sizeof(*this)); }
+        size_t &operator[](char a) { return edge[a - 'A']; }
+    };
 private:
-	bool updated;
-	vector<node> tree;
-	vector<string> pats;
+    bool updated;
+    vector<node> tree;
+    vector<string> pats;
 public:
-	trie() { reset(512, 32); }
+    trie() { reset(65536, 256); }
 private:
-	void reset(size_t tresv, size_t paresv) {
-		tree.resize(2); pats.clear(); updated = false;
-		tree.reserve(tresv); pats.reserve(paresv);
-		for (char c = 'A'; c <= 'Z'; ++c)
-			tree[0][c] = 1;
-	}
-	size_t create() {
-		tree.push_back(node());
-		return tree.size() - 1;
-	}
+    void reset(size_t tresv, size_t paresv) {
+        tree.resize(2); pats.clear(); updated = false;
+        tree.reserve(tresv); pats.reserve(paresv);
+        for (char c = 'A'; c <= 'Z'; ++c)
+            tree[0][c] = 1;
+    }
+    size_t create() {
+        tree.push_back(node());
+        return tree.size() - 1;
+    }
 public:
-	const string &pattern(size_t i) {
-		return pats[i];
-	}
-	template <typename I> size_t insert(I b, I e) {
-		size_t r = 1, paid = pats.size();
-		updated = false; pats.push_back(string());
-		for ( ; b != e; ++b) {
-			pats[paid] += *b;
-			if (tree[r][*b] == 0) {
+    const string &pattern(size_t i) const {
+        return pats[i];
+    }
+    template <typename I> size_t insert(I b, I e) {
+        size_t r = 1, paid = pats.size();
+        updated = false; pats.push_back(string());
+        for ( ; b != e; ++b) {
+            pats[paid] += *b;
+            if (tree[r][*b] == 0) {
                 size_t temp = create();
-				tree[r][*b] = temp;
+                tree[r][*b] = temp;
             }
-			r = tree[r][*b];
-		}
-		tree[r].danger = tree[r].target = true;
-		tree[r].paid = paid;
-		return paid;
-	}
-	void update() {
-		queue<size_t> qu; qu.push(1);
-		while (!qu.empty()) {
-			size_t r = qu.front(); qu.pop();
-			for (char c = 'A'; c <= 'Z'; ++c) {
-				if (tree[r][c] == 0) continue;
-				size_t p = tree[r].prev;
-				while (tree[p][c] == 0)
-					p = tree[p].prev;
-				tree[tree[r][c]].prev = tree[p][c];
-				qu.push(tree[r][c]);
-			}
-		}
-		updated = true;
-	}
-	template <typename I, typename F> size_t matches(I b, I e, F f) {
-		// typedef bool F(I i, size_t p);
-		if (!updated) update();
-		size_t p, r = 1, co = 0;
-		for ( ; b != e; ++b) {
-			while (r > 0 && tree[r][*b] == 0)
-				r = tree[r].prev;
-			r = p = tree[r][*b];
-			while (tree[p].danger) {
-				if (tree[p].target) {
-					I i = b; ++co;
-					advance(i, 1 - pats[tree[p].paid].size());
-					if (f(i, tree[p].paid)) return co;
-				}
-				p = tree[p].prev;
-			}
-		}
-		return co;
-	}
-};
-
-#include <string>
-#include <cstdlib>
-#include <cstdio>
-#include <iostream>
-#include <cstring>
-#include <ctime>
-
-using namespace std;
-
-class puzzle {
-public:
-    void reset(size_t r, size_t c) {
-        memset(matrix, 0, sizeof(matrix));
-        rows = r; cols = c;
-    }
-    size_t fi(char c, size_t i, size_t j) {
-        switch (c) {
-        case 'A': return rows-j+1;
-        case 'B': return sl_rowid(i) - j + 1;
-        case 'C': return i;
-        case 'D': return bs_rowid(i) + j - 1;
+            r = tree[r][*b];
         }
-        if (1 & (c - 'A'))
-            return fi(c - 4, i, len(i) - j + 1);
-        return fi(c - 4, i, rows - j + 1);
+        tree[r].danger = tree[r].target = true;
+        tree[r].paid = paid;
+        return paid;
     }
-    size_t fj(char c, size_t i, size_t j) {
-        switch (c) {
-        case 'A': return i;
-        case 'B': return sl_colid(i) + j - 1;
-        case 'C': return j;
-        case 'D': return bs_colid(i) + j - 1;
+    void update() {
+        queue<size_t> qu; qu.push(1);
+        while (!qu.empty()) {
+            size_t r = qu.front(); qu.pop();
+            for (char c = 'A'; c <= 'Z'; ++c) {
+                if (tree[r][c] == 0) continue;
+                size_t p = tree[r].prev;
+                while (tree[p][c] == 0)
+                    p = tree[p].prev;
+                tree[tree[r][c]].prev = tree[p][c];
+                qu.push(tree[r][c]);
+            }
         }
-        if (1 & (c - 'A'))
-            return fj(c - 4, i, len(i) - j + 1);
-        return fj(c - 4, i, rows - j + 1);
-    }
-    char &operator()(char dir, size_t i, size_t j) {
-        return matrix[fi(dir, i, j)][fj(dir, i, j)];
-    }
-    size_t size(char ch) {
-        if ((ch - 'A') & 1) return rows + cols - 1;
-        if ((ch / 2)  & 1) return cols;
-        return rows;
-    }
-    size_t len(size_t j) {
-		size_t mi = min(rows, cols), ma = max(rows, cols);
-        if (mi <= j && j <= ma) return mi;
-        if (j > ma) return mi - (j - ma);
-        return j;
-    }
-protected:
-    size_t bs_rowid(size_t i) {
-        if (i <= rows) return rows - i + 1;
-        return 1;
-    }
-    size_t bs_colid(size_t i) {
-        if (i <= rows) return 1;
-        return i - rows + 1;
-    }
-    size_t sl_rowid(size_t i) {
-        if (i <= rows) return i;
-        return rows;
-    }
-    size_t sl_colid(size_t i) {
-        if (i <= rows) return 1;
-        return i - rows + 1;
-    }
-public:
-    char matrix[1111][1111];
-    size_t rows, cols;
-};
-
-puzzle puz;
-size_t outx[1111], outy[1111];
-char outchar[1111];
-
-class addone {
-public:
-	addone(char *b, char d, size_t t) :
-		begin(b), direction(d), lineid(t) { }
-	bool operator()(char *it, size_t id) {
-		size_t index = size_t(it - begin) + 1;
-		outx[id] = puz.fi(direction, lineid, index)-1;
-		outy[id] = puz.fj(direction, lineid, index)-1;
-		outchar[id] = direction;
-		return false;
-	}
-private:
-	char *begin;
-	char direction;
-	size_t lineid;
-};
-
-char buffer[9999];
-
-int main(int argc, char *argv[]) {
-
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-
-    size_t l, c, w;
-    scanf("%lu%lu%lu", &l, &c, &w);
-
-    puz.reset(l, c);
-    for (size_t i = 1; i <= l; ++i)
-        scanf("%s", puz.matrix[i] + 1);
-
-    trie tr;
-    for (size_t i = 1; i <= w; ++i) {
-		scanf("%s", buffer);
-		char *bend = buffer + strlen(buffer);
-        tr.insert(buffer, bend);
+        updated = true;
     }
 
-    for (char ch = 'A'; ch <= 'H'; ++ch) {
-        for (size_t i = 1; i <= puz.size(ch); ++i) {
-            char *bend = buffer;
-            for (size_t j = 1; puz(ch, i, j); ++j)
-                *(bend++) = puz(ch, i, j);
-			*bend = '\0';
-            tr.matches(buffer, bend, addone(buffer, ch, i));
+    /**
+     * @brief Match the text [b, e) with the trie patterns.
+     * @param b : The begin iterator of the text.
+     * @param e : The end iterator of the text.
+     * @param f : Callback function address or object. Must be the type of
+     *            typedef bool F(I, size_t);
+     * @return The number of matched patterns before @param f returns true
+     *         (inclusive), or all matched patterns if @param f never
+     *         returns true.
+     * @date 2011-08-14
+     */
+
+    template <typename I, typename F> size_t matches(I b, I e, F f) {
+        if (!updated) update();
+        size_t p, r = 1, co = 0;
+        for ( ; b != e; ++b) {
+            while (r > 0 && tree[r][*b] == 0)
+                r = tree[r].prev;
+            r = p = tree[r][*b];
+            while (tree[p].danger) {
+                if (tree[p].target) {
+                    I i = b; ++co;
+                    advance(i, 1 - pats[tree[p].paid].size());
+                    if (f(i, tree[p].paid)) return co;
+                }
+                p = tree[p].prev;
+            }
         }
+        return co;
     }
-
-    for (size_t i = 0; i < w; ++i)
-        cout << outx[i] << ' ' << outy[i] << ' ' << outchar[i] << endl;
-
-    return EXIT_SUCCESS;
-}
+};
--- str/str.trie.simp.hpp
+++ str/str.trie.simp.hpp
@@ -1,138 +1,218 @@
-#include <iostream>
-#include <cstdlib>
-#include <utility>
+#include <iterator>
+#include <cstring>
 #include <vector>
 #include <string>
 #include <queue>
-#include <map>
-#include <set>
 
 using namespace std;
 
-#ifndef foreach
-#define foreach(i,c) for (__typeof((c).begin()) i = \
-    (c).begin(); i != (c).end(); ++i)
-#endif
-
-namespace gatelib {
-
-namespace str {
-
-template <typename T> class trie {
-
+class trie {
 private:
-
-    class node {
-    public:
-		typedef map<T, size_t> elem_t;
-		typedef typename elem_t::iterator iterator;
-		typedef typename elem_t::const_iterator const_iterator;
-        size_t prev, patt; bool danger, target; elem_t elem;
-        node() : prev(0), patt(0), danger(false), target(false) { }
-        size_t rval(T c) const {
-            const_iterator it = elem.find(c);
-            if (it == elem.end()) return 0;
-            return it->second;
-        }
-		size_t &lval(T c) { return elem[c]; }
-		const_iterator begin() const { return elem.begin(); }
-		const_iterator end() const { return elem.end(); }
-		iterator begin() { return elem.begin(); }
-		iterator end() { return elem.end(); }
-    };
-
+	struct node {
+		size_t prev, paid, edge[26];
+		bool danger, target;
+		node() { memset(this, 0, sizeof(*this)); }
+		size_t &operator[](char a) { return edge[a - 'A']; }
+	};
+private:
+	bool updated;
+	vector<node> tree;
+	vector<string> pats;
 public:
-
-    trie() : updated(false) { reset(); }
-
+	trie() { reset(512, 32); }
+private:
+	void reset(size_t tresv, size_t paresv) {
+		tree.resize(2); pats.clear(); updated = false;
+		tree.reserve(tresv); pats.reserve(paresv);
+		for (char c = 'A'; c <= 'Z'; ++c)
+			tree[0][c] = 1;
+	}
+	size_t create() {
+		tree.push_back(node());
+		return tree.size() - 1;
+	}
 public:
-
-    void reset()
-    {
-        tree.clear(); tree.resize(2); updated = false;
-        tree[0].prev = -1; tree[1].prev = 0; pat.clear();
-    }
-
-    void update()
-    {
-        deque<size_t> qu; qu.push_back(1);
-		tree[0].prev = -1;
-        while (!qu.empty()) {
-            size_t ro = qu.front(); qu.pop_front();
-            for (typename node::iterator it = tree[ro].begin(); it != tree[ro].end(); ++it) {
-                size_t pr = tree[ro].prev, i = it->second;
-                while (pr != -1) {
-                    if (tree[pr].rval(it->first) != -1) {
-                        tree[i].prev = tree[pr].rval(it->first);
-                        tree[i].danger = tree[i].danger || tree[tree[i].prev].danger;
-                        break;
-                    } else pr = tree[pr].prev;
-                }
-                qu.push_back(it->second);
+	const string &pattern(size_t i) {
+		return pats[i];
+	}
+	template <typename I> size_t insert(I b, I e) {
+		size_t r = 1, paid = pats.size();
+		updated = false; pats.push_back(string());
+		for ( ; b != e; ++b) {
+			pats[paid] += *b;
+			if (tree[r][*b] == 0) {
+                size_t temp = create();
+				tree[r][*b] = temp;
             }
-        }
-        updated = true;
-    }
-
-    void dump() {
-        for (size_t i = 0; i < tree.size(); ++i) {
-            printf("N%d,P%d:", i, tree[i].prev);
-            for (typename node::iterator it = tree[i].begin(); it != tree[i].end(); ++it)
-				printf(" %c%d", it->first, it->second);
-			printf("\n");
-        }
-    }
-
-public:
-
-	template <typename it> void insert(it begin, it end) {
-		size_t ro = 1; updated = false;
-        pat.push_back(string());
-		for ( ; begin != end; ++begin) {
-            if (tree[ro].rval(*begin) == 0)
-                tree[ro].lval(*begin) = create();
-            ro = tree[ro].rval(*begin);
-            tree[0].lval(*begin) = 1;
-            pat[pat.size()-1] += *begin;
-        }
-        tree[ro].danger = tree[ro].target = true;
-        tree[ro].patt = pat.size() - 1;
-    }
-
-    template <typename I> pair< I, vector<size_t> > find(I begin, I end) {
-        size_t p = 1; if (!updated) update();
-        for ( ; begin != end; ++begin) {
-            while (p > 0 && tree[p].rval(*begin) == 0)
-                p = tree[p].prev;
-			p = tree[p].rval(*begin);
-			if (p < 1) p = 1;
-            if (tree[p].danger) {
-                size_t q = p; vector<size_t> v;
-                while (q && tree[q].danger) {
-                    if (tree[q].target)
-                        v.push_back(tree[q].patt);
-                    q = tree[q].prev;
-                }
-                return make_pair(begin, v);
-            }
-        }
-        return make_pair(begin, vector<size_t>());
-    }
-
-private:
-
-    size_t create() {
-        tree.push_back(node());
-        return tree.size() - 1;
-    }
-
-private:
-
-    deque<node> tree;
-    bool updated;
-    vector<string> pat;
+			r = tree[r][*b];
+		}
+		tree[r].danger = tree[r].target = true;
+		tree[r].paid = paid;
+		return paid;
+	}
+	void update() {
+		queue<size_t> qu; qu.push(1);
+		while (!qu.empty()) {
+			size_t r = qu.front(); qu.pop();
+			for (char c = 'A'; c <= 'Z'; ++c) {
+				if (tree[r][c] == 0) continue;
+				size_t p = tree[r].prev;
+				while (tree[p][c] == 0)
+					p = tree[p].prev;
+				tree[tree[r][c]].prev = tree[p][c];
+				qu.push(tree[r][c]);
+			}
+		}
+		updated = true;
+	}
+	template <typename I, typename F> size_t matches(I b, I e, F f) {
+		// typedef bool F(I i, size_t p);
+		if (!updated) update();
+		size_t p, r = 1, co = 0;
+		for ( ; b != e; ++b) {
+			while (r > 0 && tree[r][*b] == 0)
+				r = tree[r].prev;
+			r = p = tree[r][*b];
+			while (tree[p].danger) {
+				if (tree[p].target) {
+					I i = b; ++co;
+					advance(i, 1 - pats[tree[p].paid].size());
+					if (f(i, tree[p].paid)) return co;
+				}
+				p = tree[p].prev;
+			}
+		}
+		return co;
+	}
 };
 
-} // namespace str
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <ctime>
 
-} // namespace gatelib
+using namespace std;
+
+class puzzle {
+public:
+    void reset(size_t r, size_t c) {
+        memset(matrix, 0, sizeof(matrix));
+        rows = r; cols = c;
+    }
+    size_t fi(char c, size_t i, size_t j) {
+        switch (c) {
+        case 'A': return rows-j+1;
+        case 'B': return sl_rowid(i) - j + 1;
+        case 'C': return i;
+        case 'D': return bs_rowid(i) + j - 1;
+        }
+        if (1 & (c - 'A'))
+            return fi(c - 4, i, len(i) - j + 1);
+        return fi(c - 4, i, rows - j + 1);
+    }
+    size_t fj(char c, size_t i, size_t j) {
+        switch (c) {
+        case 'A': return i;
+        case 'B': return sl_colid(i) + j - 1;
+        case 'C': return j;
+        case 'D': return bs_colid(i) + j - 1;
+        }
+        if (1 & (c - 'A'))
+            return fj(c - 4, i, len(i) - j + 1);
+        return fj(c - 4, i, rows - j + 1);
+    }
+    char &operator()(char dir, size_t i, size_t j) {
+        return matrix[fi(dir, i, j)][fj(dir, i, j)];
+    }
+    size_t size(char ch) {
+        if ((ch - 'A') & 1) return rows + cols - 1;
+        if ((ch / 2)  & 1) return cols;
+        return rows;
+    }
+    size_t len(size_t j) {
+		size_t mi = min(rows, cols), ma = max(rows, cols);
+        if (mi <= j && j <= ma) return mi;
+        if (j > ma) return mi - (j - ma);
+        return j;
+    }
+protected:
+    size_t bs_rowid(size_t i) {
+        if (i <= rows) return rows - i + 1;
+        return 1;
+    }
+    size_t bs_colid(size_t i) {
+        if (i <= rows) return 1;
+        return i - rows + 1;
+    }
+    size_t sl_rowid(size_t i) {
+        if (i <= rows) return i;
+        return rows;
+    }
+    size_t sl_colid(size_t i) {
+        if (i <= rows) return 1;
+        return i - rows + 1;
+    }
+public:
+    char matrix[1111][1111];
+    size_t rows, cols;
+};
+
+puzzle puz;
+size_t outx[1111], outy[1111];
+char outchar[1111];
+
+class addone {
+public:
+	addone(char *b, char d, size_t t) :
+		begin(b), direction(d), lineid(t) { }
+	bool operator()(char *it, size_t id) {
+		size_t index = size_t(it - begin) + 1;
+		outx[id] = puz.fi(direction, lineid, index)-1;
+		outy[id] = puz.fj(direction, lineid, index)-1;
+		outchar[id] = direction;
+		return false;
+	}
+private:
+	char *begin;
+	char direction;
+	size_t lineid;
+};
+
+char buffer[9999];
+
+int main(int argc, char *argv[]) {
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    size_t l, c, w;
+    scanf("%lu%lu%lu", &l, &c, &w);
+
+    puz.reset(l, c);
+    for (size_t i = 1; i <= l; ++i)
+        scanf("%s", puz.matrix[i] + 1);
+
+    trie tr;
+    for (size_t i = 1; i <= w; ++i) {
+		scanf("%s", buffer);
+		char *bend = buffer + strlen(buffer);
+        tr.insert(buffer, bend);
+    }
+
+    for (char ch = 'A'; ch <= 'H'; ++ch) {
+        for (size_t i = 1; i <= puz.size(ch); ++i) {
+            char *bend = buffer;
+            for (size_t j = 1; puz(ch, i, j); ++j)
+                *(bend++) = puz(ch, i, j);
+			*bend = '\0';
+            tr.matches(buffer, bend, addone(buffer, ch, i));
+        }
+    }
+
+    for (size_t i = 0; i < w; ++i)
+        cout << outx[i] << ' ' << outy[i] << ' ' << outchar[i] << endl;
+
+    return EXIT_SUCCESS;
+}
